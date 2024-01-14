f4715ea2ed02cb584e8b6e9ccd2afcf6fa7c3a18
Kadir Zukorlic <kadirzukorlic@gmail.com>
2024-01-14 01:06:24 +0100

    Use param and body decorators (param to access url param, and body to post body)

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessage(@Body('content') body: any) {
+    console.log(body);
+    return body;
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return `hello i am message with ID: ${id}`;
+  }
+}
